6af010198208859fa105f2d6ba2835e4d121a8a5
german-webdev <125234299+german-webdev@users.noreply.github.com>
2023-03-31 11:22:02 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,17 @@
 
-function sequenceSum(begin, end) {
-    let total = getDigitsSum(begin, end)
-
-    return begin > end ? NaN : total;
+function getStringCount(object) {
+    if (typeof object == 'string') return 1;
+    if (!object) return 0;
+    return Object.values(object).reduce((acc, cur) => acc + getStringCount(cur), 0);
 }
 
-function getSum(arr) {
-    let sum = 0;
+console.log(getStringCount
+({
+  first: '1',
+  second: '2',
+  third: false,
+  fourth: ['anytime', 2, 3, 4 ],
+  fifth:  null,
+})); // 3
 
-    for ( let i = 0; i < arr.length; i++ ) {
-        sum += arr[i];
-    }
-    
-    return sum;
-}
-
-function getArrayOfDigits(begin, end) {
-    let arr = [];
-
-    for ( let i = begin; i <= end; i++ ) {
-        arr.push(i);
-    }
-
-    return arr;
-}
-
-function getDigitsSum(begin, end) {
-    return getSum(getArrayOfDigits(begin, end));
-}
-
-
-console.log(sequenceSum(1, 5)); // 1 + 2 + 3 + 4 + 5 = 15
-console.log(sequenceSum(4, 10)); // 4 + 5 + 6 + 7 + 8 + 9 + 10 = 49
-console.log(sequenceSum(-3, 2)); // (-3) + (-2) + (-1) + 0 + 1 + 2 = -3
-console.log(sequenceSum(7, 2)); // NaN (т.к. это "пустая" последовательность)
-console.log(sequenceSum(0, 0)); // 0 (т.к. это единственное число, входящее в последовательность)
-console.log(sequenceSum(6, 6)); // 6 (т.к. это единственное число, входящее в последовательность)
-
+console.log(getStringCount(['1', '2', ['3']])); // 3
