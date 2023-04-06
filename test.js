a4c43e0afa6e675887ce0a0ffb9a8f6fd20a80b2
german-webdev <125234299+german-webdev@users.noreply.github.com>
2023-04-06 20:19:57 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,33 @@
-const data = [
-  { 
-    firstName: 'Emily', 
-    lastName: 'N.', 
-    country: 'Ireland', 
-    continent: 'Europe', 
-    age: 30, 
-    language: 'Ruby' 
-  },
-
-  { 
-    firstName: 'Nor', 
-    lastName: 'E.', 
-    country: 'Malaysia', 
-    continent: 'Asia', 
-    age: 20, 
-    language: 'Clojure' 
+const firstObject = {
+  a: {
+    b: {
+      c: 1,
+      d: 'string',
+      e: {
+        num: 1
+      }
+    }
   }
-];
+};
 
-const createUsernames = users => {
-  function getYearOfBirth(age) {
-    return new Date().getFullYear() - age;
+const secondObject = {
+  a: {
+    b: {
+      e: {
+        num: 1,
+      },
+      d: 'string',
+      c: 1,
+    }
   }
+};
 
-  users.forEach(e => e.username = `${e.firstName}${e.lastName.split('.').join('')}${getYearOfBirth(e.age)}`.toLowerCase());
-  
-  return users;
+function deepEqual (obj1, obj2) {
+
 }
-const processedData = createUsernames(data);
-console.log(processedData); // [
-// { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby',
-//  username: 'emilyn1990' },
-// { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure',
-//  username: 'nore2000' }
-// ];
+
+
+console.log(deepEqual(firstObject, secondObject)); // true
+console.log(deepEqual({ a: 1, b: 3 }, { b: 2, a: 1})); // false
+console.log(deepEqual(1, 2)); // false
+console.log(deepEqual(true, false)); // false
